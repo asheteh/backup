56a26322e799917b5028a32f73f7ee7b8d332f11
root <abhijeets7661@gmail.com>
2020-09-17 11:20:24 +0000

    new back up

---
+++ urls.py
@@ -0,0 +1,59 @@
+
+from django.contrib import admin
+from django.urls import path,include
+from django.conf.urls import url
+from pages import views
+from django.contrib.sitemaps.views import sitemap
+from .sitemaps import StaticViewSitemap
+sitemaps ={
+    'static': StaticViewSitemap
+}
+
+
+
+
+urlpatterns = [
+    url(r'^$',views.index,name='index'),
+    url(r'^ads\.txt',views.ads,name='ads'),
+    path('ccat-predac-course-pune',views.about,name='ccat-predac-course-pune'),
+    path('sitemap.xml',sitemap,{'sitemaps':sitemaps}),
+    path('ccat',views.ccat,name='ccat'),
+    path('C-CAT-2019-Predac-Course',views.crashcourse,name='C-CAT-2019-Predac-Course'),
+    path('cdac',views.cdac,name='cdac'),
+    path('ccee',views.ccee,name='ccee'),
+    path('test',views.test,name='test'),
+    path('search',views.search,name='search'),
+    path('rank',views.rank,name='rank'),
+    path('payment/',include('payment.urls')),
+    path('OnlineTest/',include('OnlineTest.urls')),
+    path('accounts/',include('accounts.urls')),
+    path('admin/', admin.site.urls),
+    path('interview',views.interview,name='interview'),
+    path('notify',views.notify,name='notify'),
+    path('ccat_notify',views.ccat_notify,name='ccat_notify'),
+    path('questions',views.questions,name='questions'),
+    path('ccat_questions',views.ccat_questions,name='ccat_questions'),
+    path('previous',views.prev_questions,name='previous'),
+    path('CPP/',include('CPP.urls')),
+    path('C/',include('C.urls')),
+    path('English-Tutorial',views.eng,name='English-Tutorial'),
+    path('dac_interview',views.dac_interview,name='dac_interview'),
+    path('big_data_interview',views.bd_interview,name='big_data_interview'),
+    path('Aptitude-Tutorial',views.apti,name='Aptitude-Tutorial'),
+    path('music/',include('music.urls')),
+    path('guitar/',include('guitar.urls')),
+    path('C-CPP-Objective-Questions',views.section_b,name='C-CPP-Objective-Questions'),
+    path('C-CPP-Programming-Questions',views.coding,name='C-CPP-Programming-Questions'),
+    path('cdac-courses',views.other,name='cdac-courses'),
+    path('dmc-cdac-course',views.dmc,name='dmc-cdac-course'),
+    path('iot-cdac-course',views.iot,name='iot-cdac-course'),
+    path('desd-cdac-course',views.desd,name='desd-cdac-course'),
+    path('dittis-cdac-course',views.dittis,name='dittis-cdac-course'),
+    path('hpcsa-cdac-course',views.hpcsa,name='hpcsa-cdac-course'),
+    path('vlsi-cdac-course',views.vlsi,name='vlsi-cdac-course'),
+    path('ai-cdac-course',views.ai,name='ai-cdac-course'),
+    path('cdac-c-cat-syllabus',views.syllabus,name='cdac-c-cat-syllabus'),
+    path('cdac-c-cat-result',views.result,name='cdac-c-cat-result'),
+    path('cdac-c-cat-admit-card',views.admit,name='cdac-c-cat-admit-card'),
+]
+ 
